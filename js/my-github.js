a56e4c89228a84aeeab8b666f0c7276aa38f9715
Soha <s.azadi12319@gmail.com>
2018-04-19 16:11:10 +0100

    my repo

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,19 @@
 // Write code here to communicate with Github
-
+var url = "https://api.github.com/users/azadi11/repos";
+fetch(url)
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(repos) {
+    var repoList = document.querySelector("#repos-list");
+    var reposCount = document.querySelector("#repos-count");
+    reposCount.innerText = repos.length;
+    repos.forEach(function(repo) {
+      var itemLink = document.createElement("a");
+      itemLink.setAttribute("href", repo.html - url);
+      itemLink.innerText = repo.name;
+      var listItems = document.createElement("li");
+      listItems.appendChild(itemLink);
+      repoList.appendChild(listItems);
+    });
+  });
